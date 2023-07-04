2ac784bfca9122c80fe713e83d69acb0f2c1e6a8
Saket Kothari <saketkothari722@gmail.com>
2023-07-04 09:53:40 +0530

    Resolver Functions

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+import db from './_db.js';
+
 // types
 import { typeDefs } from './schema.js';
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
 
 // Server Setup
 const server = new ApolloServer({
   typeDefs,
-  // resolvers
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
